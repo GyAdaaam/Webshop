2c99cf5f91e2b45df10bbda711c0e49153058f3d
Gyányi Ádám <161303221+GyAdaaam@users.noreply.github.com>
2024-07-05 11:01:27 +0000

    v1

--- FRONTEND/src/components/MyNavbar/MyNavbar.jsx
+++ FRONTEND/src/components/MyNavbar/MyNavbar.jsx
@@ -14,8 +14,9 @@ export default function MyNavbar() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobileView(window.innerWidth <= 768);
-      setShowAuthButtons(window.innerWidth <= 768);
+      const mobileView = window.innerWidth <= 768;
+      setIsMobileView(mobileView);
+      setShowAuthButtons(mobileView);
     };
 
     window.addEventListener('resize', handleResize);
@@ -28,15 +29,23 @@ export default function MyNavbar() {
   return (
     <Navbar expand="lg" className="w-75 m-3 rounded-4 ms-auto me-auto headercard">
       <Container fluid>
-        <Navbar.Brand href="/" className='mynavbar-navbarlink'><img src="images/MyNavbar/logo.png" className='float-right' width={40}/> Videójátékbolt </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" className='custom-toggler navbar-toggler ml-auto' style={{color: "white"}}>
-          <img src="images/MyNavbar/hamburger.png" width={20}/>
+        <Navbar.Brand as={Link} to="/" className="mynavbar-navbarlink">
+          <img src="images/MyNavbar/logo.png" className="float-right" width={40} alt="Logo" /> Videójátékbolt
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNavAltMarkup" className="custom-toggler navbar-toggler ml-auto">
+          <img src="images/MyNavbar/hamburger.png" width={20} alt="Menu" />
         </Navbar.Toggle>
         <Navbar.Collapse id="navbarNavAltMarkup">
           <Nav className="me-auto my-2 my-lg-0 mynavbar-navbarlink" navbarScroll>
-            <Nav.Link as={Link} to="/aruhaz" className='mynavbar-navbarlink'>Játékok</Nav.Link>
-            <Nav.Link as={Link} to="/kapcsolat" className='mynavbar-navbarlink'>Kapcsolat</Nav.Link>
-            <Nav.Link as={Link} to="/rolunk" className='mynavbar-navbarlink'>Rólunk</Nav.Link>
+            <Nav.Link as={Link} to="/aruhaz" className="mynavbar-navbarlink">
+              Játékok
+            </Nav.Link>
+            <Nav.Link as={Link} to="/kapcsolat" className="mynavbar-navbarlink">
+              Kapcsolat
+            </Nav.Link>
+            <Nav.Link as={Link} to="/rolunk" className="mynavbar-navbarlink">
+              Rólunk
+            </Nav.Link>
           </Nav>
           <Nav className="ms-auto">
             {showAuthButtons && (
@@ -57,15 +66,20 @@ export default function MyNavbar() {
                 >
                   Regisztráció
                 </Button>
-                <Dropdown show={dropdownOpen} onToggle={toggleDropdown} drop="start">
+                <Dropdown
+                  show={dropdownOpen}
+                  onToggle={toggleDropdown}
+                  drop="start"
+                  className="my-dropdown"
+                >
                   <Dropdown.Toggle variant="link" id="dropdown-basic" className="text-white ml-auto">
-                    <img src='./images/MyNavbar/user3.png' alt="User Icon" width={35} />
+                    <img src="./images/MyNavbar/user3.png" alt="User Icon" width={35} />
                   </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#" className="btn-light rounded-3 mt-1 mb-1 border-1 border-secondary">
+                  <Dropdown.Menu className="dropdown-menu-right animated fadeIn faster" style={{ backgroundColor: '#252525', marginLeft: '-10px' }}>
+                    <Dropdown.Item href="#" className="dropdown-item-custom text-white">
                       Bejelentkezés
                     </Dropdown.Item>
-                    <Dropdown.Item href="#" className="btn-light rounded-3 mt-1 mb-1 border-1 border-secondary">
+                    <Dropdown.Item href="#" className="dropdown-item-custom text-white">
                       Regisztráció
                     </Dropdown.Item>
                   </Dropdown.Menu>
@@ -73,15 +87,20 @@ export default function MyNavbar() {
               </>
             )}
             {!isMobileView && (
-              <Dropdown show={dropdownOpen} onToggle={toggleDropdown} drop="down">
+              <Dropdown
+                show={dropdownOpen}
+                onToggle={toggleDropdown}
+                drop="down"
+                className="my-dropdown"
+              >
                 <Dropdown.Toggle variant="link" id="dropdown-basic" className="text-white ml-auto">
-                  <img src='./images/MyNavbar/user3.png' alt="User Icon" width={35} />
+                  <img src="./images/MyNavbar/user3.png" alt="User Icon" width={35} />
                 </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#" className="btn-light rounded-3 mt-1 mb-1 border-1 border-secondary">
+                <Dropdown.Menu className="dropdown-menu-right animated fadeIn faster" style={{ backgroundColor: '#1F1F23', marginLeft: '-100px' }}>
+                  <Dropdown.Item href="#" className="dropdown-item-custom text-white">
                     Bejelentkezés
                   </Dropdown.Item>
-                  <Dropdown.Item href="#" className="btn-light rounded-3 mt-1 mb-1 border-1 border-secondary">
+                  <Dropdown.Item href="#" className="dropdown-item-custom text-white">
                     Regisztráció
                   </Dropdown.Item>
                 </Dropdown.Menu>
