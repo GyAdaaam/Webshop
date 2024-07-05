62b9ed769010d5e3a3036dc96c69279e34a73bcf
Gyányi Ádám <161303221+GyAdaaam@users.noreply.github.com>
2024-07-05 11:27:54 +0000

    v6

--- FRONTEND/src/components/Home/Home.jsx
+++ FRONTEND/src/components/Home/Home.jsx
@@ -1,18 +1,28 @@
-// Ez a JS modul a kezdőoldalt megjelenítő komponens.
-
-// A szükséges komponensek importálása.
+// Szükséges komponensek importálása.
 import "./Home.css"; // A Home komponens saját stíluslapja.
 import { Link } from 'react-router-dom'; // Az oldalak közötti váltáshoz szükséges komponens.
+ // Példa kép importálása (ezt a képet cseréld ki a saját képeddel).
 
 // A kezdőoldalt megjelenítő komponens.
 export default function Home() {
   return (
-    <div className="d-flex justify-content-center home-background">
+    <div className="d-flex justify-content-center align-items-center home-background">
       <div className="container-fluid home-container">
-        <div className="text-center text-light home-text">
-          <h1 className="display-4 fw-bolder mb-3">JátékBazár</h1>
-          <p className="lead fw-normal display-6 mb-3">- Üdvözlünk a weboldalon -</p>
-          <Link to='/aruhaz'><button type="button" className="btn btn-danger ribbon fw-bolder w-100 home-button">{"<-- Indulhat a felfedezés -->"}</button></Link>
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            <div className="text-center text-light home-text">
+              <h1 className="display-4 fw-bolder mb-3">JátékBazár</h1>
+              <p className="lead fw-normal display-6 mb-3">- Üdvözlünk a weboldalon -</p>
+              <Link to='/aruhaz'>
+                <button type="button" className="btn btn-danger ribbon fw-bolder w-100 home-button">
+                  {"<-- Indulhat a felfedezés -->"}
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <img src='images/MyNavbar/home.png' alt="Játék kép" className="img-fluid" />
+          </div>
         </div>
       </div>
     </div>
